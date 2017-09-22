c25ce15663e9dc9a7aea59154a80731a126473bd
陈嘉涵 <chenjiahan@meituan.com>
2017-09-22 14:11:53 +0800

    optimize table style

--- src/component/Content.vue
+++ src/component/Content.vue
@@ -105,14 +105,14 @@ export default {
     p > code,
     li > code,
     .zan-doc-table code {
-      margin: 0 2px;
+      margin: 2px;
       padding: 2px 7px;
       display: inline-block;
       vertical-align: middle;
     }
 
     .zan-doc-table code {
-      padding: 0 10px;
+      padding: 0 8px;
     }
   }
 }
