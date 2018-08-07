c7fdbb88bff3fbc4165f7ebf6e789c14dec1bccd
陈嘉涵 <chenjiahan@youzan.com>
2018-08-07 15:53:07 +0800

    [Build] format webpack config

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,30 +1,27 @@
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.vue$/,
-                use: ['vue-loader']
-            },
-            {
-                test: /\.(css|pcss)$/,
-                use: ['css-loader', 'postcss-loader']
-            },
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.vue', '.css'],
-        alias: {
-            vue$: 'vue/dist/vue.esm.js'
-        }
-    },
-    performance: {
-        hints: false
-    },
-    resolve: {
-        extensions: ['.webpack.js',  '.js', '.vue', '.ts']
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.vue$/,
+        use: ['vue-loader']
+      },
+      {
+        test: /\.(css|pcss)$/,
+        use: ['css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.vue', '.css'],
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
     }
+  },
+  performance: {
+    hints: false
+  }
 };
--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -4,27 +4,27 @@ const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
 require('shelljs/global');
 
-env.NODE_ENV = 'production';
+process.env.NODE_ENV = 'production';
 
 module.exports = merge(baseWebpackConfig, {
-    entry: {
-        index: './src/index.js'   
-    },
-    output: {
-        path: path.resolve(__dirname, '../dist'),
-        publicPath: "dist/",
-        filename: "[name].js",
-        libraryTarget: 'commonjs2'
-    },
-    externals: {
-        vue: 'vue'
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.ModuleConcatenationPlugin()
-    ]
-})
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: 'dist/',
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    vue: 'vue'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ]
+});
