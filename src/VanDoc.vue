6c858657f8100676a808e2f4b3ea5e46a615944a
陈嘉涵 <chenjiahan@youzan.com>
2019-02-02 09:53:23 +0800

    add doc nav

--- src/VanDoc.vue
+++ src/VanDoc.vue
@@ -37,6 +37,79 @@ export default {
       type: String,
       default: ''
     }
+  },
+
+  data() {
+    return {
+      nav: [],
+      currentPath: null,
+      leftNav: null,
+      rightNav: null
+    };
+  },
+
+  watch: {
+    '$route.path'() {
+      this.setNav();
+      this.updateNav();
+    }
+  },
+
+  created() {
+    this.setNav();
+    this.updateNav();
+    this.keyboardHandler();
+  },
+
+  methods: {
+    setNav() {
+      const nav = this.config.nav;
+      for (let i = 0; i < nav.length; i++) {
+        const navItem = nav[i];
+        if (!navItem.groups) {
+          this.nav.push(nav[i]);
+        } else {
+          for (let j = 0; j < navItem.groups.length; j++) {
+            this.nav = this.nav.concat(navItem.groups[j].list);
+          }
+        }
+      }
+    },
+
+    updateNav() {
+      let currentIndex;
+      this.currentPath = '/' + this.$route.path.split('/').pop();
+      for (let i = 0, len = this.nav.length; i < len; i++) {
+        if (this.nav[i].path === this.currentPath) {
+          currentIndex = i;
+          break;
+        }
+      }
+      this.leftNav = this.nav[currentIndex - 1];
+      this.rightNav = this.nav[currentIndex + 1];
+    },
+
+    handleNavClick(direction) {
+      const nav = direction === 'prev' ? this.leftNav : this.rightNav;
+      if (nav.path) {
+        this.$router.push(this.base + nav.path);
+      } else if (nav.link) {
+        window.location.href = nav.link;
+      }
+    },
+
+    keyboardHandler() {
+      window.addEventListener('keyup', event => {
+        switch (event.keyCode) {
+          case 37: // left
+            this.handleNavClick('prev');
+            break;
+          case 39: // right
+            this.handleNavClick('next');
+            break;
+        }
+      });
+    }
   }
 };
 </script>
