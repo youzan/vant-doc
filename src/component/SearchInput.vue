a492aecc8fadf23b31daca8832596fc28b3e0f69
陈嘉涵 <chenjiahan@youzan.com>
2019-05-28 11:31:05 +0800

    feat: custom search result style

--- src/component/SearchInput.vue
+++ src/component/SearchInput.vue
@@ -43,6 +43,8 @@ export default {
 </script>
 
 <style lang="postcss">
+@import '../style/variable';
+
 .van-doc-search {
   height: 60px;
   width: 200px;
@@ -59,6 +61,44 @@ export default {
   &::placeholder {
     opacity: 0.7;
     color: #fff;
+  }
+}
+
+.ds-dropdown-menu {
+  line-height: 1.8;
+}
+
+.algolia-autocomplete {
+  .algolia-docsearch-suggestion--highlight {
+    color: $van-doc-blue;
+    background-color: transparent;
+  }
+
+  .algolia-docsearch-suggestion--title {
+    font-weight: 500;
+  }
+
+  .algolia-docsearch-suggestion--text {
+    .algolia-docsearch-suggestion--highlight {
+      box-shadow: inset 0 -1px 0 0 $van-doc-blue;
+    }
+  }
+
+  .algolia-docsearch-suggestion--category-header {
+    border-bottom-color: #eee;
+  }
+
+  .ds-dropdown-menu [class^='ds-dataset-'] {
+    border: none;
+  }
+
+  .ds-dropdown-menu {
+    top: 80% !important;
+    box-shadow: 0 4px 12px #ebedf0;
+
+    &:before {
+      display: none;
+    }
   }
 }
 </style>
